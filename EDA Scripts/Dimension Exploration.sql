cd9b8d093930f03c373c806f2ff7e95244cd52e6
PurviMishra <purvimish8july@gmail.com>
2026-04-13 20:30:30 +0530

    Files for EDA and data quality check in silver and gold layer

---
+++ EDA Scripts/Dimension Exploration.sql
@@ -0,0 +1,7 @@
+-- Explore All Countries our customers come from
+SELECT DISTINCT country FROM gold.dim_customers
+
+--Explore All Categories "The major divisions"
+SELECT DISTINCT category,subcategory,product_name FROM gold.dim_products
+ORDER BY 1,2,3
+
